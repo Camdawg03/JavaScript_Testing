9c60345370bbf40cf31438f2d003cf1db7087efe
Camdawg03 <133887963+Camdawg03@users.noreply.github.com>
2025-02-20 16:46:11 +0000

    ScrollReveal

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 
-ScrollReveal().reveal('.image-cat1', {delay: 1000});
-
-ScrollReveal().reveal('.image-cat2', { duration: 500 });
+window.onload = function (){
 
 
+}
+
+ScrollReveal().reveal('.image-cat2', {delay: 1000});
+
+ScrollReveal().reveal('.image-cat2', {delay: 1500});
+ScrollReveal().reveal('.image-cat1', {
+    rotate: {
+        x: 90,
+        z: 90
+        
+    }
+});
+
+ScrollReveal().reveal('.BaileeandI',{distance: '70px' });
+ScrollReveal().reveal('.BaileeandI', {delay: 1700});
+
+ScrollReveal().reveal('.Work', { scale: 4 });
+ScrollReveal().reveal('.Work', {delay: 2200});
+
+
+ScrollReveal().reveal('.Frens', { easing: 'ease-in' });
+ScrollReveal().reveal('.Frens', {delay: 2300});
